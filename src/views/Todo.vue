198adeb47dd597ab66a9588eb496070c5645bf42
littlebugyang <im.lby@qq.com>
2020-07-26 10:46:53 +0800

    1. Add 'copy to clipboard'; 2. Show notifications when time is up; 3. I want to rectify one committed message that I haven't finished the 'U' of the tasks.

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -40,6 +40,7 @@
                 <!-- View the remaining tasks -->
                 <h4 class="mb-5">
                     <span>Remaining Tasks</span>
+                    <base-button type="primary" @click="exportTasks">export</base-button>
                 </h4>
 
                 <task-item v-for="task in undoneTasks" :key="task.id" :done=false :id="task.id" :title="task.title"
@@ -129,7 +130,7 @@
 
                 // The three variables below are about countdown, but there's no need to store them.
                 showCountdown: false,
-                presetCountdownMinutes: ['1', '25', '35', '45'],
+                presetCountdownMinutes: ['25', '35', '45'],
                 intervalId: 0,
 
                 // The variables below are about countdown. They should be stored in local storage.
@@ -137,7 +138,7 @@
                 countdown: {
                     taskId: -1,
                     startTime: 0,
-                    minutes: '1'
+                    minutes: '25'
                 },
                 countdowns: [],
 
@@ -192,6 +193,16 @@
                     if (_this.countdownExpired(Date.now())) {
                         _this.saveCountdown()
                         _this.cancelCountdown()
+
+                        // show notification
+                        if (window.Notification && Notification.permission !== 'denied') {
+                            Notification.requestPermission(function (status) {
+                                new Notification("Tomato Countdown", {
+                                    body: "Time is up!!!!",
+                                    icon: 'http://img3.imgtn.bdimg.com/it/u=3891266103,2994336694&fm=26&gp=0.jpg'
+                                })
+                            })
+                        }
                     } else {
                         let secondsLeft = _this.countdown.minutes * 60.0 - (Date.now() - _this.countdown.startTime) / 1000.0
                         let percentage = (1.0 - secondsLeft / (_this.countdown.minutes * 60.0)) * 100
@@ -240,12 +251,74 @@
                 this.saveToLocalStorage('tasks', JSON.stringify(this.tasks))
                 this.saveToLocalStorage('accumulatedId', this.accumulatedId)
             },
+            exportTasks: function () {
+                var textArea = document.createElement("textarea");
+
+                //
+                // *** This styling is an extra step which is likely not required. ***
+                //
+                // Why is it here? To ensure:
+                // 1. the element is able to have focus and selection.
+                // 2. if element was to flash render it has minimal visual impact.
+                // 3. less flakyness with selection and copying which **might** occur if
+                //    the textarea element is not visible.
+                //
+                // The likelihood is the element won't even render, not even a
+                // flash, so some of these are just precautions. However in
+                // Internet Explorer the element is visible whilst the popup
+                // box asking the user for permission for the web page to
+                // copy to the clipboard.
+                //
+
+                // Place in top-left corner of screen regardless of scroll position.
+                textArea.style.position = 'fixed';
+                textArea.style.top = 0;
+                textArea.style.left = 0;
+
+                // Ensure it has a small width and height. Setting to 1px / 1em
+                // doesn't work as this gives a negative w/h on some browsers.
+                textArea.style.width = '2em';
+                textArea.style.height = '2em';
+
+                // We don't need padding, reducing the size if it does flash render.
+                textArea.style.padding = 0;
+
+                // Clean up any borders.
+                textArea.style.border = 'none';
+                textArea.style.outline = 'none';
+                textArea.style.boxShadow = 'none';
+
+                // Avoid flash of white box if rendered for any reason.
+                textArea.style.background = 'transparent';
+
+                let toBeCopied = ''
+                for (let i = 0; i < this.tasks.length; ++i) {
+                    let task = this.tasks[i]
+                    toBeCopied += `- [${task.done ? 'x' : ' '}] ${task.title}\n`
+                }
+                textArea.value = toBeCopied
+
+                document.body.appendChild(textArea);
+                textArea.focus();
+                textArea.select();
+
+                try {
+                    var successful = document.execCommand('copy');
+                    var msg = successful ? 'successful' : 'unsuccessful';
+                    console.log('Copying text command was ' + msg);
+                } catch (err) {
+                    console.log('Oops, unable to copy');
+                }
+
+                document.body.removeChild(textArea);
+            },
             handleNewTaskInput: function (val) {
                 this.newTitle = val
             },
             handleTaskToggle: function (id) {
                 let i = this.getTaskIndexById(id)
                 this.tasks[i].done = !this.tasks[i].done
+                this.saveToLocalStorage('tasks', JSON.stringify(this.tasks))
             },
             handleTaskDeleted: function (id) {
                 console.log("Delete task item with id: ", id)
